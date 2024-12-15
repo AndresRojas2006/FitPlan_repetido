772229cfa9992abe1c6f06083ab7dd334e5774aa
AndresRojas <andres2007benavides@gmail.com>
2024-12-15 18:21:24 -0500

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { User1Component } from './user1/user1.component';
+import { SecondComponent } from './second/second.component';
+import { ThirdComponent } from './third/third.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+{path: '',  component: User1Component},
+{path: 'second', component: SecondComponent},
+{path: 'third', component: ThirdComponent}
+];
